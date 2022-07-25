ab6b812176eb61e0e8a4f253ec4a72fe4ad3261a
Alfred Masekwameng <97237252+Lepoto@users.noreply.github.com>
2022-07-26 01:00:29 +0200

    Delete filters.py

--- UniApi/API/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from django_filters import filters
-from career.models import Bursary
-
-
-class BursaryFilter(django_filters.FilterSet):
-    class Meta:
-        model = Bursary
-        fields = ['bursary_name', 'bursary_field_study_1']
